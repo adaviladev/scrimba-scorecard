1ecc52b81f2f95d67115ac35da3fae05f0a4f877
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-07-13 20:11:35 -0400

    refactored

--- script.js
+++ script.js
@@ -4,41 +4,65 @@ let guestStoreEl = document.getElementById("guest-score");
 let homeScore = 0;
 let guestScore = 0;
 
-function add1Home() {
-    homeScore += 1
-    homeStoreEl.textContent = homeScore;
-    console.log("Nice!")
-}
 
-function add2Home() {
-    homeScore += 2
-    homeStoreEl.textContent = homeScore;
-    console.log("Nice!")
-}
+document.addEventListener("click", function(event) {
+    let homeScoreEl = document.getElementById("home-score")
+    let homeScore = parseInt(homeScoreEl.textContent);
 
-function add3Home() {
-    homeScore += 3
-    homeStoreEl.textContent = homeScore;
-    console.log("Nice!")
-}
+    let guestScoreEl = document.getElementById("guest-score")
+    let guestScore = parseInt(guestScoreEl.textContent);
 
-function add1Guest() {
-    guestScore += 1
-    guestStoreEl.textContent = guestScore;
-    console.log("Oh no!")
-}
+    if (event.target.id === "add-one-home") {
+        homeScoreEl.textContent = homeScore + 1
+    } else if (event.target.id === "add-two-home") {
+        homeScoreEl.textContent = homeScore + 2
+    } else if (event.target.id === "add-three-home") {
+        homeScoreEl.textContent = homeScore + 3
+    } else if (event.target.id === "add-one-guest") {
+        guestScoreEl.textContent = guestScore + 1
+    } else if (event.target.id === "add-two-guest") {
+        guestScoreEl.textContent = guestScore + 2
+    } else if (event.target.id === "add-three-guest") {
+        guestScoreEl.textContent = guestScore + 3
+    }
+        
+});
 
-function add2Guest() {
-    guestScore += 2
-    guestStoreEl.textContent = guestScore;
-    console.log("Oh no!")
-}
+// function add1Home() {
+//     homeScore += 1
+//     homeStoreEl.textContent = homeScore;
+//     console.log("Nice!")
+// }
 
-function add3Guest() {
-    guestScore += 3
-    guestStoreEl.textContent = guestScore;
-    console.log("Oh no!")
-}
+// function add2Home() {
+//     homeScore += 2
+//     homeStoreEl.textContent = homeScore;
+//     console.log("Nice!")
+// }
+
+// function add3Home() {
+//     homeScore += 3
+//     homeStoreEl.textContent = homeScore;
+//     console.log("Nice!")
+// }
+
+// function add1Guest() {
+//     guestScore += 1
+//     guestStoreEl.textContent = guestScore;
+//     console.log("Oh no!")
+// }
+
+// function add2Guest() {
+//     guestScore += 2
+//     guestStoreEl.textContent = guestScore;
+//     console.log("Oh no!")
+// }
+
+// function add3Guest() {
+//     guestScore += 3
+//     guestStoreEl.textContent = guestScore;
+//     console.log("Oh no!")
+// }
 
 function resetScore() {
     homeScore = 0;
